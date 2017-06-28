2960afdbb77c416b2b383ca88382074ef8a0301d
byung-u <iam.byungwoo@gmail.com>
2017-06-28 20:08:05 +0900

    ADD solutions

--- p1_100.py
+++ p1_100.py
@@ -625,3 +625,13 @@ def p15():
     print(result)
 
 
+'''
+Problem 16
+
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 21000?
+
+'''
+def p16():
+    print(sum(map(int, str(2 ** 1000))))
+    
