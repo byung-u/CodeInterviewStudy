504d795deef366865af4f3c15d4c3b6401d5b5da
byung-u <iam.byungwoo@gmail.com>
2017-08-22 16:45:40 +0900

    Add @youth4ever solution with Pythagorean Triplets

--- 100_to_199/euler_138.py
+++ 100_to_199/euler_138.py
@@ -10,6 +10,7 @@ By using the Pythagorean theorem it can be seen that the height of the triangle,
 With b = 272 and L = 305, we get h = 273, which is one more than the base length, and this is the second smallest isosceles triangle with the property that h = b ± 1.
 Find ∑ L for the twelve smallest isosceles triangles for which h = b ± 1 and b, L are positive integers.
 '''
+from itertools import count
 from decimal import Decimal
 from math import modf
 
@@ -47,4 +48,35 @@ def p138():
     print(sum(result))
 
 
-p138()
+'''
+@youth4ever solution
+Using the Pythagorean Triplets and remarking that if we use :
+h = m**2-n**2
+b = 2*m*n
+L = m**2 + n**2
+we can start with (m,n) = (4,1) next sequence will be :
+(m,n)= (17, 4), then (m,n)= (72, 17), (m,n)=(305, 72)... and so on.
+
+Therefore the pattern becomes obvious. I will not insist on that. Here is the solution, about 10 ms in Python3 :
+
+'''
+def special_Isosceles_Tiangles(how_many) :
+    cnt, S = 0, 0
+    m, n = 4, 1
+    while True :
+        for m in count(m, 1):
+            h = m ** 2 - n ** 2
+            b = 2 * m * n
+            L = m ** 2 + n ** 2
+            if b * 2 == h + 1 or b * 2== h - 1:
+                cnt += 1
+                print('(m,n)=' ,m,n,'        (h, b, L)= ', (h, b ,L) )
+                S += L
+                break
+        n = m
+        m = int(4.236 * n)
+        if cnt == how_many :
+            return print('\nAnswer :', S)
+
+special_Isosceles_Tiangles(12)
+# p138()
